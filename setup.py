3301a6cdc184c3c5cc3f56bfc438ef1b21e4a3a9
Emmanuel-kwizera <kwizemmy2004@gmail.com>
2025-06-10 23:03:57 +0200

    refac: updated setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="alumathpeergroup14",
     version="1.0.0",
-    description="A friendly matrix multiplication package by Kabango Mathias.",
+    description="A matrix multiplication package by Peer Group 14.",
     author="Emmanuel Kwizera, Jade Isimbi Tuzinde, Pretty Ntakirutimana, Phinah Mahoro",
     author_email="e.kwizera1@alustudent.com",
     packages=find_packages(),
