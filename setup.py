7a0fa1aefb07888acdab8c0c11188dc9d3135ce2
phinah <phinah.mahoro10@gmail.com>
2025-06-10 22:19:34 +0200

    feat: added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+setup(
+    name="alumathpeergroup14",
+    version="1.0.0",
+    description="A friendly matrix multiplication package by Kabango Mathias.",
+    author="Emmanuel Kwizera, Jade Isimbi Tuzinde, Pretty Ntakirutimana, Phinah Mahoro",
+    author_email="e.kwizera1@alustudent.com",
+    packages=find_packages(where="src"),
+    package_dir={"":"src"},
+    include_package_data=True,
+    python_requires=">=3.6"
+)
